d83d2cf8b4f8d7e8b967e079a5c70756eb5a025a
brandoncwang <72431306+brandoncwang@users.noreply.github.com>
2022-02-25 18:33:34 -0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>2022 © Brandon Wang.</a></div>
+      <div className={styles.copyright}>2022 © Brandon Wang. Thanks for scrolling all the way down. </div>
 
       {hasMounted ? (
         <div className={styles.settings}>
